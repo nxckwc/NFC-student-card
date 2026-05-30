255d7197bf583a523fde6c4085c797f9ccc9a8f3
nxckwc <nxckforwork@gmail.com>
2026-05-31 00:38:07 +0700

    refactor : improve file structure of navbar

--- frontend/src/app/component/Navbar/NavTab.tsx
+++ frontend/src/app/component/Navbar/NavTab.tsx
@@ -4,6 +4,7 @@ import GeneralNavButton from './GeneralNavButton'
 import LoginButton from './LoginButton'
 import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
+import  Link  from 'next/link';
 
 
 const NavTab = () => {
@@ -14,7 +15,13 @@ const NavTab = () => {
     <div>
       <div className={'w-fit h-10 flex items-center justify-center text-white p-1 rounded-md gap-1'}>
         <GeneralNavButton />
-        {!isLoginRoute && <LoginButton />}
+        {!isLoginRoute && <Link href="/login">
+              <button 
+                className={'w-fit h-fit flex items-center justify-center bg-red-500 p-2 rounded-md font-bold cursor-pointer text-white hover:scale-105 transition duration-300'} 
+              >
+                Login
+              </button>
+          </Link>}
       </div>
       
     </div>
--- frontend/src/app/component/Navbar/Navbar.tsx
+++ frontend/src/app/component/Navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import IconLabel from './IconLabel'
+
 import NavTab from './NavTab'
 
 const Navbar = () => {
   return (
     <div className={'fixed top-0 left-0 w-full z-50 backdrop-blur-sm'}>
         <div className={'flex min-h-10 min-w-full items-center justify-between p-2'}>
-            <IconLabel/>
+            <img src="/images-removebg-preview (1) (1).png" alt="icon" className={'h-12 w-12'} />
             <NavTab/>
         </div>
     </div>
