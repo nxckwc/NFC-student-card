843749df286cb05a435f98aa582064b34b8757fd
Hadi <fk.hadi.yt@gmail.com>
2026-05-31 07:31:17 +0200

    Navbar now into [locale]/

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -2,8 +2,8 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
-import Navbar from "../component/Navbar/Navbar";
 import { IBM_Plex_Sans_Thai, IBM_Plex_Sans } from "next/font/google";
+import Navbar from "./navbar/Navbar";
 
 const ibmPlexThai = IBM_Plex_Sans_Thai({
   weight: ['400', '700'],
