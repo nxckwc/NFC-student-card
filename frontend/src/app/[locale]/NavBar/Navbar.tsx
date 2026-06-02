003f53fc192944a640c0573bb4cc37c01b6d1776
nxckwc <nxckforwork@gmail.com>
2026-06-02 19:45:35 +0700

    add Contact button on navbar, adjust styling of the lang swap button

--- frontend/src/app/[locale]/NavBar/Navbar.tsx
+++ frontend/src/app/[locale]/NavBar/Navbar.tsx
@@ -22,10 +22,11 @@ const Navbar = () => {
         <div className={'flex min-h-10 min-w-full items-center justify-between p-2'}>
             <img src="/images-removebg-preview (1) (1).png" alt="icon" className={'h-12 w-12'} />
             
-            <div className={'w-fit h-10 flex items-center justify-center text-white p-1 rounded-md gap-1'}>
-              <div className={'flex-1 flex items-center justify-center gap-2'}>
+            <div className={'w-fit h-10 flex items-center justify-center text-white p-1 rounded-md gap-3'}>
+              <div className={'flex-1 flex items-center justify-center gap-3'}>
                 <Link href="/docs" className={'w-fit p-1 cursor-pointer hover:underline transition duration-300'}>Docs</Link>
                 <Link href="/about" className={'w-fit p-1 cursor-pointer hover:underline transition duration-300'}>About</Link>
+                <Link href="/contact" className={'w-fit p-1 cursor-pointer hover:underline transition duration-300'}>Contact</Link>
               </div>
               {!isLoginRoute && <Link href="/login">
                     <button 
@@ -34,9 +35,9 @@ const Navbar = () => {
                       Login
                     </button>
                 </Link>}
-              <button className='px-2 py-1 rounded border border-white/20 text-sm'
+              <button className='px-3 py-1 rounded-2xl border border-white/20 text-sm flex items-center justify-center cursor-pointer hover:bg-white hover:text-black transition duration-300'
                 onClick={toggleLocale}>
-                  { locale === 'en' ? 'TH' : 'EN'}
+                  { locale === 'en' ? 'EN' : 'TH'}
               </button>
             </div>
         </div>
