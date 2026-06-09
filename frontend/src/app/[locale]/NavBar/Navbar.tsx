f1cafc48074aeafbfda1c177404cb155b83fd67e
nxckwc <nxckforwork@gmail.com>
2026-06-09 21:47:21 +0700

    feat:basic dashboard

--- frontend/src/app/[locale]/NavBar/Navbar.tsx
+++ frontend/src/app/[locale]/NavBar/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className={'fixed top-0 left-0 w-full z-50 backdrop-blur-sm'}>
+    <div className={'fixed top-0 left-0 w-full z-50 backdrop-blur-sm border-b-gray-300/10 border'}>
         <div className={'flex min-h-10 min-w-full items-center justify-between p-2'}>
             <img src="/images-removebg-preview (1) (1).png" alt="icon" className={'h-12 w-12'} />
             
